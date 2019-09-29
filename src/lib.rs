217d887e5e80453e48825829c172d11832d4c91e
Tom Fagerland <tom.fagerland@entur.org>
2019-09-29 13:30:32 +0200

    Upgrade

--- src/lib.rs
+++ src/lib.rs
@@ -6,43 +6,37 @@ const AHPLA: &str = "zyxwvutsrqponmlkjihgfedcba";
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
     let encoded_string = codec(plain);
-    let result = add_spaces(encoded_string.as_str());
-
-    result.trim().to_string()
+    add_spaces(&encoded_string)
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    return codec(cipher);
+    codec(cipher)
 }
 
 fn codec(text: &str) -> String {
-    let mut alphabet: HashMap<char, char> = HashMap::new();
-    ALPHA.chars().zip(AHPLA.chars()).for_each(|(k, v)| {
-        alphabet.insert(k, v);
-    });
-
+    let alphabet: HashMap<char, char> = ALPHA.chars().zip(AHPLA.chars()).collect();
     let mut result = String::new();
     for x in text.chars() {
         if x.is_ascii_alphabetic() {
             let c = x.to_ascii_lowercase();
-            let encoded_char: &char = alphabet
+            let encoded_char = alphabet
                 .get(&c)
-                .expect(format!("character {} not found in array", &c).as_ref());
+                .unwrap_or_else(|| panic!(format!("character {} not found in array", &c)));
             result.push(*encoded_char);
         } else if x.is_ascii_alphanumeric() && x != ' ' {
             result.push(x);
         }
     }
-    result.trim().to_string()
+    result
 }
 
 fn add_spaces(string: &str) -> String {
-    string.chars().enumerate().fold(String::new(), |acc, (count, c)|{
+    string.chars().enumerate().fold(String::new(), |mut acc, (count, c)|{
        if count %5 == 0{
-           format!("{} {}", acc, c)
-       } else {
-           format!("{}{}", acc, c)
+           acc.push(' ');
        }
-    })
+        acc.push(c);
+        acc
+    }).trim().to_string()
 }
